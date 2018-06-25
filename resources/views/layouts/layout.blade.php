4148489661d24e32e987d1b96ddf007faf66cc36
alokin95 <nikola_m95@hotmail.com>
2018-06-25 05:03:24 +0200

    Laravel specific tooling

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    </head>
+    <body>
+
+    <div id="app">
+        @include('layouts.header')
+
+        <div class="section">
+        
+            <div class="container">
+
+                <router-view></router-view>
+
+            </div>
+
+        </div>
+
+    </div>
+        
+
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="tabs is-boxed">
+      <div class="container">
+        <ul>
+
+          <router-link to="/" tag="li" exact>
+            <a>Home</a>
+          </router-link>
+
+          <router-link to="/add" tag="li">
+            <a>Add user</a>
+          </router-link>
+          
+        </ul>
+      </div>
+    </nav>
