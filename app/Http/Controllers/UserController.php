866479f39bbec2552e4e023d504d4f20255a6a25
alokin95 <nikola_m95@hotmail.com>
2018-06-25 05:01:31 +0200

    API for fetching, deleting users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+
+    public function allUsers()
+    {
+        return User::all();
+    }
+
+    public function addUser()
+    {
+        
+        $user = request()->validate([
+            'name' => 'required|min:5|max:30',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'password' => 'required|confirmed'
+        ]);
+
+        $new_user = new User($user);
+
+        $new_user->save();
+    }
+
+    public function deleteUser()
+    {
+        
+        User::destroy(request()->id);
+    }
+}
