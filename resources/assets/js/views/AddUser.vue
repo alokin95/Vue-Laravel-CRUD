b610f0848577d758f01f6447837be0dd40bc6627
alokin95 <nikola_m95@hotmail.com>
2018-06-26 23:43:39 +0200

    Removed password confirmation markup

--- resources/assets/js/views/AddUser.vue
+++ resources/assets/js/views/AddUser.vue
@@ -39,18 +39,6 @@
   </div>
 </div>
 
-<div class="field">
-  <label class="label">Confirm password</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input" type="password" placeholder="Confirm password" value="" name="password_confirmation" v-model="user.password_confirmation">
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </div>
-</div>
 <div class="field is-grouped">
   <div class="control">
     <button class="button is-link">Submit</button>
@@ -74,8 +62,7 @@
                 axios.post('user', {
                     name: this.user.name,
                     email: this.user.email,
-                    password: this.user.password,
-                    password_confirmation: this.user.password_confirmation
+                    password: this.user.password
                 })
                 .then(function (response) {
                     self.$router.push({
