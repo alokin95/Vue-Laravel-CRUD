62a92fcc726792112ce69ee60ed2ddd38378bfed
alokin95 <nikola_m95@hotmail.com>
2018-06-27 04:40:25 +0200

    Added modal for user info

---
+++ resources/assets/js/views/UserInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="isVisible" class="modal is-active">
+    <div class="modal-background">
+
+    </div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modal title</p>
+          <button class="delete" aria-label="close" @click="close"></button>
+        </header>
+        <section class="modal-card-body">
+        <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save changes</button>
+          <button class="button">Cancel</button>
+        </footer>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+    data(){
+      return {
+        isVisible: true
+      }
+    },
+
+    methods: {
+      
+      close()
+      {
+        this.isVisible = false;
+      }
+    }
+  }
+</script>
