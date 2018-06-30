1eae070b4e827ebcbb2d4487ca32044fdbbbee63
alokin95 <nikola_m95@hotmail.com>
2018-06-30 04:50:17 +0200

    Including custom css

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,9 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>VUE SPA</title>
+        <title>VUE Singe page application</title>
                 <!-- Styles -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         
     </head>
     <body>
