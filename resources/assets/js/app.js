ff92816c342ee495100be817159dbd5ce27f0371
alokin95 <nikola_m95@hotmail.com>
2018-06-30 04:48:49 +0200

    Importing vue paginator

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,9 @@ window.Vue = require('vue');
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VuePaginate from 'vue-paginate'
+
+Vue.use(VuePaginate)
 Vue.use(VueRouter)
 
 import router from './routes'; // Routes from routes.js file
